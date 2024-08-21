32d80b2f4956fb040a03c3879570f7f25470616c
Wavin Dev <eshwavin@gmail.com>
2024-08-21 19:59:10 -0400

    Initial Commit

---
+++ SmartSign Dictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartSign Dictionary
+//
+//  Created by Srivinayak Chaitanya Eshwa on 21/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
